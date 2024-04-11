a743b2faf87f5af973fb0afae8617f316c5fad60
Sldark23 <115877460+Sldark23@users.noreply.github.com>
2024-04-11 01:38:05 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,28 +2,11 @@ document.addEventListener('DOMContentLoaded', () => {
   fetch('products.json')
     .then(response => response.json())
     .then(data => {
-      const productsContainer = document.getElementById('products-container');
-
       data.forEach(product => {
-        const productCard = document.createElement('div');
-        productCard.classList.add('product-card');
-
-        const productName = document.createElement('div');
-        productName.classList.add('product-name');
-        productName.textContent = product.name;
-
-        const productPrice = document.createElement('div');
-        productPrice.classList.add('product-price');
-        productPrice.textContent = `Preço: ${product.price}`;
-
-        const buyButton = document.createElement('button');
-        buyButton.classList.add('buy-button');
-        buyButton.textContent = 'Comprar';
-
-        productCard.appendChild(productName);
-        productCard.appendChild(productPrice);
-        productCard.appendChild(buyButton);
-        productsContainer.appendChild(productCard);
+        const scriptElement = document.createElement('script');
+        scriptElement.src = product.buyButtonUrl;
+        scriptElement.classList.add('ymp-script');
+        document.body.appendChild(scriptElement);
       });
     })
     .catch(error => console.error('Erro ao carregar produtos:', error));
