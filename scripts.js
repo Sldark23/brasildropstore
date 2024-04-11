29ae374384e9372c12729a204774dea27ebef7ff
Sldark23 <115877460+Sldark23@users.noreply.github.com>
2024-04-11 01:41:28 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,11 +2,13 @@ document.addEventListener('DOMContentLoaded', () => {
   fetch('products.json')
     .then(response => response.json())
     .then(data => {
+      const productsContainer = document.getElementById('products-container');
+
       data.forEach(product => {
-        const scriptElement = document.createElement('script');
-        scriptElement.src = product.buyButtonUrl;
-        scriptElement.classList.add('ymp-script');
-        document.body.appendChild(scriptElement);
+        const productHtml = product.htmlCode;
+        const productElement = document.createElement('div');
+        productElement.innerHTML = productHtml;
+        productsContainer.appendChild(productElement);
       });
     })
     .catch(error => console.error('Erro ao carregar produtos:', error));
